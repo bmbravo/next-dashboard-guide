874818be7047480e61a2ea598e4ab764616c4bf2
Bill Bravo <bbravo@ec.krugercorp.com>
2024-09-24 21:38:00 -0500

    chapter 15 to 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: { template: '%s | Acme Dashboard', default: 'Acme Dashboard' },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-dashboard-guide.vercel.app'),
+};
 
 export default function RootLayout({
   children,
